c5241ab74fedcd434318f59a7e145942d1d3aaa1
UBnkantah <90422770+UBnkantah@users.noreply.github.com>
2023-04-04 01:40:10 +0100

    start

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1 className='HEDING'>TASKIFY</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/InputField.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const InputField = () => {
+  return (
+    <form>
+        <input type="text" name="" id="" placeholder='Enter a Task'
+            className='input__btn'
+        />
+        <button type='submit' className='input__submit'>Go</button>
+    </form>
+    
+  )
+}
+
+export default InputField
